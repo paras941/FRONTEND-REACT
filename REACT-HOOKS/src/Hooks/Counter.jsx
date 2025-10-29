c24d2a758271877dbc8fa006957c9c0816735ca8
paras <rawatparas@776gmail.com>
2025-10-29 23:29:16 +0530

    REACT-JOURNEY

---
+++ REACT-HOOKS/src/Hooks/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import "./Counter.css";
+
+const Counter =() =>{
+    const [ count,setCount ]= useState(0);
+    return(
+        <div className='Container'>
+            <p id='para'>You can clicked 0 times </p>
+            <button id='btn' onClick={()=>{
+                   setCount(count+1)
+            }} > Click here</button>
+        </div>
+        
+    );
+} ;
+
+export default Counter;
